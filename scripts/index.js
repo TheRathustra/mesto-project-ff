63c0d28a5eac8a7d7da31ec978325084282d5e47
Konstantin Maksimov <Purple-Pleb@yandex.ru>
2024-06-01 14:25:03 +0300

    feat: Вывод карточек и удаление

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
-// @todo: Темплейт карточки
+//DOM узлы
+const content = document.querySelector('.content');
+const addButton = content.querySelector('.profile__add-button');
+const places = content.querySelector('.places');
+const placesList = places.querySelector('.places__list');
 
-// @todo: DOM узлы
+//Функция создания карточки
+function addCard(name, link) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
 
-// @todo: Функция создания карточки
+  const deleteBtn = cardElement.querySelector('.card__delete-button');
+  deleteBtn.addEventListener('click', function(evt) {
+    deleteCard(evt.target.parentElement);
+  });
 
-// @todo: Функция удаления карточки
+  cardElement.querySelector('.card__image').setAttribute('src', link);
+  cardElement.querySelector('.card__title').textContent = name;
 
-// @todo: Вывести карточки на страницу
+  placesList.append(cardElement);
+}
+
+function deleteCard(card) {
+  placesList.removeChild(card);
+}
+
+//Вывести карточки на страницу
+initialCards.forEach((card) => addCard(card.name, card.link));
